8599c49fc46deed6e6f586db01c36c029dc2ad1b
kayerson96 <108358349+kayerson96@users.noreply.github.com>
2022-09-13 12:22:32 -0500

    se agrego

---
+++ src/components/header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import  Button from 'react-bootstrap/Button';
+import './Header.css';
+import labial04 from '../images/labial04.png'
+import { AiFillAlipayCircle } from "react-icons/ai";
+
+function Header(){
+    return (
+        <div className='header'>
+            <p>Hola</p>
+            <img src={labial04}/>
+            <AiFillAlipayCircle/>
+        </div>
+        
+    )
+}
+export default Header;
