04ef2189a50f56014a188d9bf06b29327cbfcc47
kayerson96 <108358349+kayerson96@users.noreply.github.com>
2022-09-20 13:03:03 -0500

    funciona la importacion

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,15 +2,11 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import  Button from 'react-bootstrap/Button';
 import './Header.css';
-import labial04 from '../images/labial04.png'
-import { AiFillAlipayCircle } from "react-icons/ai";
 
 function Header(){
     return (
         <div className='header'>
-            <p>Hola anderson jose</p>
-            <img src={labial04}/>
-            <AiFillAlipayCircle/>
+            <p>Hola anderson jose are here</p>
         </div>
         
     )
---
+++ src/pages/home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from '../../components/header/Header';
+
+
+function Home(){
+    return (
+        <div>
+            <p>Hola anderson jose</p>
+            <Header/>
+        </div>
+        
+        
+    )
+}
+export default Home;
