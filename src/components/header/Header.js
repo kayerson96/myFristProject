4ef2332eb1aaeaa177ea5cdc9bc243f9d1417526
kayerson96 <108358349+kayerson96@users.noreply.github.com>
2022-09-20 21:45:00 -0500

    se dio color al navbar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Header.css';
-import Navbar from 'react-bootstrap/esm/Navbar';
+import"./Header.css";
 
 function Header(){
     return (
         <div>
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <nav class="navbar navbar-expand-lg ">
   <div class="container-fluid">
-    <a class="navbar-brand" href="Home.js">Home</a>
+    <a class="nav-link" href="Home.js">Home</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="Works.js">Works</a>
+          <a class="nav-link" aria-current="page" href="Works.js">Works</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="Aboutme.js">About Me</a>
