94745eea25add04a4422470755b17e5034da0273
kayerson96 <108358349+kayerson96@users.noreply.github.com>
2022-10-02 23:53:00 -0500

    se agrego espacio entre redes sociales

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -10,7 +10,7 @@ function Contact() {
           <h1>Anderson Jose Castillo Muñoz</h1>
           <h4>Correo: anderson.castillo196@gmail.com</h4>
           <h4>Telefono: 3017707943</h4>
-          <h4>Redes Sociales: <br></br>
+          <h4>Redes Sociales: <br></br><br></br>
           <Button href="https://github.com/kayerson96" className ='circulogit1' ></Button>
             <Button href="" className='circuloinst1'></Button>
             <Button href="" className='circuloface1'></Button>
