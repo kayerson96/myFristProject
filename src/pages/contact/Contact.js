b02c9937343f0af02bf2f6628197096dc6d5ea12
kayerson96 <108358349+kayerson96@users.noreply.github.com>
2022-10-02 23:32:04 -0500

    se termino pagina de contacto

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import "./Contact.css";
+import Button from 'react-bootstrap/Button';
 
 
 function Contact() {
     return (
       <>
-        <div>ANDERSON</div>
+        <div className='containerContacto row'>
+          <h1>Anderson Jose Castillo Muñoz</h1>
+          <h4>Correo: anderson.castillo196@gmail.com</h4>
+          <h4>Telefono: 3017707943</h4>
+          <h4>Redes Sociales: <br></br>
+          <Button href="https://github.com/kayerson96" className ='circulogit1' ></Button>
+            <Button href="" className='circuloinst1'></Button>
+            <Button href="" className='circuloface1'></Button>
+            <Button href="" className='circulomail1'></Button>
+            </h4>
+          
+        </div>
       </>
     );
   }
